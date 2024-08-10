e5272ba48bb445ff1e2c29ef5147457631bd7158
Saalim <115459936+saalim-khan@users.noreply.github.com>
2024-08-10 10:33:51 +0530

    Update Application_TF.vars.tf

--- Application_TF.vars.tf
+++ Application_TF.vars.tf
@@ -7,6 +7,6 @@ module "ecs" {
   cluster_service_task_name = "django-notes-app-task"
   vpc_id_subnet_list      = ["subnet-00e7bb95b770045a0", "subnet-0afb4cdf359c0816f", "subnet-06160d4dcb973b682", "subnet-03bf8a900d6535134"]
   execution_role_arn      = "arn:aws:iam::891376966913:role/ecsTaskExecutionRole"
-  image_id                = "891376966913.dkr.ecr.us-east-1.amazonaws.com/notes_app:596562ffbf887c8ff93925e2dae3ef8edf13caa9"
+  image_id                = "891376966913.dkr.ecr.us-east-1.amazonaws.com/notes_app:db9bbc5af0a471a97a2504cf2f567e07632c6c11"
 }
 
